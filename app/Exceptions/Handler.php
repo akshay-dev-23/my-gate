a2f8482045f49f3617d8a07889a4e1bc9d534955
Akshay <akshay.fullstack.dev@gmail.com>
2023-10-21 23:01:48 +0530

    erroe handle

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -27,19 +27,19 @@ class Handler extends ExceptionHandler
     {
 
         $this->renderable(function (Exception $exc, Request $request) {
-            // if ($request->is('api/*')) {
-            //     $message = '';
-            //     if ($exc->getCode() == 0 || $exc->getCode() == 500 || $exc->getCode() == null) {
-            //         $message = "Something went wrong.Please try again later";
-            //         $code = 500;
-            //     } else {
-            //         $message = $exc->getMessage();
-            //         $code = $exc->getCode();
-            //     }
-            //     return response()->json([
-            //         'message' => $message
-            //     ], $code);
-            // }
+            if ($request->is('api/*')) {
+                $message = '';
+                if ($exc->getCode() == 0 || $exc->getCode() == 500 || $exc->getCode() == null) {
+                    $message = "Something went wrong.Please try again later";
+                    $code = 500;
+                } else {
+                    $message = $exc->getMessage();
+                    $code = $exc->getCode();
+                }
+                return response()->json([
+                    'message' => $message
+                ], $code);
+            }
         });
     }
 }
